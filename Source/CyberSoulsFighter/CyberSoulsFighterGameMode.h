e7c1f06805d7afedfef8babee483e1acb3678614
Felixs123 <101900450+Felixs123@users.noreply.github.com>
2023-11-03 11:16:30 -0400

    untrack files in git ignore

---
+++ Source/CyberSoulsFighter/CyberSoulsFighterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CyberSoulsFighterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACyberSoulsFighterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACyberSoulsFighterGameMode();
+};
+
+
+
